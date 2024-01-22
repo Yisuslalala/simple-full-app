6046487560595d60b640795a1b201a0362b742bd
Yisuslalala <wardodev1.00@gmail.com>
2024-01-21 23:40:52 -0600

    button and spacing style finished

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -16,25 +16,32 @@ const Home = () => {
   return (
     <main>
       <section className="padding-block-900">
-        <div className="container">
+        <div className="container center-text">
           <div className="columns">
             <div>
-              <h1 className="primary-heading fw-bold">ajad jewelry shop</h1>
-              <p>En Ajad, la elegancia cobra vida con un brillo único.</p>
+              <div>
+                <h1 className="primary-heading fw-bold">ajad jewelry shop</h1>
+                <p className="padding-400">
+                  En Ajad, la elegancia cobra vida con un brillo único.
+                </p>
+              </div>
               <div className="image-container">
                 <img src={FirstImage} alt="MainImage" className="image" />
               </div>
             </div>
             <div className="inverted-column">
-              <div className="image-container image">
+              <div className="image-container inverted-image">
                 <img src={SecondImage} alt="SecondImage" className="image" />
               </div>
-              <p className="fw-semi-bold text">
-                Descubre el arte de la elegancia en Ajad, donde cada joya es una
-                obra maestra que ilumina tu estilo con un brillo excepcional.
-              </p>
-              <div className="button">
-                <button>Tienda</button>
+              <div className="inverted-text">
+                <p className="fw-semi-bold padding-400">
+                  Descubre el arte de la elegancia en Ajad, donde cada joya es
+                  una obra maestra que ilumina tu estilo con un brillo
+                  excepcional.
+                </p>
+              </div>
+              <div className="inverted-button padding-400">
+                <button className="button">Tienda</button>
               </div>
             </div>
           </div>
