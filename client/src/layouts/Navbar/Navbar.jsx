abb0a0ec0512edb714a9b5acc5a4dd1cadbdfcac
Yisuslalala <wardodev1.00@gmail.com>
2024-01-28 00:38:23 -0600

    first version of navbar

--- client/src/layouts/Navbar/Navbar.jsx
+++ client/src/layouts/Navbar/Navbar.jsx
@@ -4,13 +4,22 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar-container">
-      <ul className="menu">
-        <NavbarItem itemName={"Home"} path={"/"} />
-        <NavbarItem itemName={"About"} path={"/about"} />
-        <NavbarItem itemName={"Shop"} path={"/shop"} />
-      </ul>
-    </nav>
+    <div>
+      <nav className="navbar-container">
+        <ul className="menu">
+          <NavbarItem itemName={"Home"} path={"/"} />
+          <NavbarItem itemName={"About"} path={"/about"} />
+          <NavbarItem itemName={"Shop"} path={"/shop"} />
+        </ul>
+        {/**
+          <ul className="menu vertical-menu">
+            <NavbarItem itemName={"Home"} path={"/"} />
+            <NavbarItem itemName={"About"} path={"/about"} />
+            <NavbarItem itemName={"Shop"} path={"/shop"} />
+          </ul>
+          */}
+      </nav>
+    </div>
   );
 };
 
