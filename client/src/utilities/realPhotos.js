4307bb884fb5dbbc23c013e85f0d4c6fbb6b8171
Yisuslalala <wardodev1.00@gmail.com>
2024-02-24 15:23:58 -0600

    First version of display images

---
+++ client/src/utilities/realPhotos.js
@@ -0,0 +1,45 @@
+import PendientesFlores from "../assets/images/flores/IMG_0660.jpeg";
+import PendientesLisos from "../assets/images/lisos/IMG_0649.jpeg";
+import PendientesPerlas from "../assets/images/perlas/IMG_0658.jpeg";
+import PendientesTrenzados from "../assets/images/trenzados/IMG_0603.jpeg";
+
+export const realPhotos = [
+  {
+    id: 1,
+    name: "Pendientes de Flores",
+    price: 180,
+    dimensions: "Diámetro: 1.5 centímetros",
+    weight: "7 gramos",
+    material: "'Acero inoxidable",
+    photo: PendientesFlores,
+  },
+  {
+    id: 2,
+    name: "Pendientes Lisos",
+    price: 150,
+    dimensions: "Diámetro: 2.5 centímetros",
+    weight: "15 gramos",
+    material: "Acero inoxidable",
+    photo: PendientesLisos,
+  },
+  {
+    id: 3,
+    name: "Pendientes de Perlas",
+    price: 200,
+    dimensions: "Diámetro: 1.5 centímetros",
+    weight: "5 gramos",
+    material: "Acero inoxidable",
+    photo: PendientesPerlas,
+  },
+  {
+    id: 4,
+    name: "Pendientes Trenzados",
+    price: 180,
+    dimensions: "Diámetro: 3 centímetros",
+    weight: "15 gramos",
+    material: "Acero inoxidable",
+    photo: PendientesTrenzados,
+  },
+];
+
+export default realPhotos;
