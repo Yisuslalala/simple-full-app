fd010759b71eaf45680d5b2cd405f5d1877a8335
Yisuslalala <wardodev1.00@gmail.com>
2024-02-24 15:28:27 -0600

    Test stuff removed or changed

--- client/src/components/ProdutList/ProductList.jsx
+++ client/src/components/ProdutList/ProductList.jsx
@@ -1,17 +1,24 @@
-import fakeProducts from "../../utilities/fakeProducts";
+import realPhotos from "../../utilities/realPhotos";
+
+import "./ProductList.css";
 
 export const ProductList = () => {
-  const listProducts = fakeProducts.map((product) => (
-    <li key={product.id}>
-      <img src={product.picture} />
-      <p>{product.name}</p>
-      {console.log(product.picture)}
-    </li>
+  const listProducts = realPhotos.map((product) => (
+    <div key={product.id} className="pl-item">
+      <img src={product.photo} className="pl-image" />
+      <div className="pl-text">
+        <p className="pl-name">{product.name}</p>
+        {console.log(product.photo)}
+        <p className="pl-price">{product.price}</p>
+      </div>
+    </div>
   ));
   return (
-    <article>
-      <h3>Productos</h3>
-      {listProducts}
+    <article className="pl padding-block-900">
+      <div className="pl-title">
+        <h3 className="secondary-heading fw-semi-bold">Productos</h3>
+      </div>
+      <div className="pl-images">{listProducts}</div>
     </article>
   );
 };
