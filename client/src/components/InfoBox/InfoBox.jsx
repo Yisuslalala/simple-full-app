a6c155226ead5579229c712b38bf3510f9136815
Yisuslalala <wardodev1.00@gmail.com>
2024-03-13 00:46:19 -0600

    New component InfoBox was added

---
+++ client/src/components/InfoBox/InfoBox.jsx
@@ -0,0 +1,27 @@
+import "./InfoBox.css";
+
+function InfoBox() {
+  return (
+    <div className="padding-block-900 color-accent-50">
+      <div className="container center-text">
+        <h3 className="primary-heading">Joyería a tu medida</h3>
+        <div className="columns">
+          <div className="padding-400">
+            <p>
+              "Adéntrate en el encanto eterno de nuestras joyas de calidad
+              excepcional. Cada pieza refleja el amor y la dedicación de una
+              familia que ha pasado generaciones perfeccionando su arte.
+              Descubre la calidez y la autenticidad en cada detalle, y lleva
+              contigo un pedacito de hogar dondequiera que vayas."
+            </p>
+          </div>
+          <div className="padding-top-400">
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default InfoBox;
