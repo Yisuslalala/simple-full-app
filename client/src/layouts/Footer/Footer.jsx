ab3c52e366b2d799a141118bddec3a0fcb2ce72d
Yisuslalala <wardodev1.00@gmail.com>
2024-03-16 01:19:51 -0600

    Footer style sheet created

--- client/src/layouts/Footer/Footer.jsx
+++ client/src/layouts/Footer/Footer.jsx
@@ -1,10 +1,12 @@
+import "./Footer.css";
+
 const Footer = () => {
   return (
     <section className="padding-block-900 color-accent-900">
       <div className="container">
         <div>
           <div>
-            <h1 className="primary-heading">Hello</h1>
+            <h1 className="primary-heading footer-text-color">Hello</h1>
           </div>
         </div>
       </div>
