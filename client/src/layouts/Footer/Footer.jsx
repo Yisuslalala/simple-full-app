23fb5c840395e81d4c7784633d299673a1f68f0c
Yisuslalala <wardodev1.00@gmail.com>
2024-03-20 00:11:22 -0600

    Footer component initializate

--- client/src/layouts/Footer/Footer.jsx
+++ client/src/layouts/Footer/Footer.jsx
@@ -1,12 +1,31 @@
 import "./Footer.css";
 
+import InstagramIcon from "@mui/icons-material/Instagram";
+
 const Footer = () => {
   return (
     <section className="padding-block-900 color-accent-900">
       <div className="container">
-        <div>
-          <div>
-            <h1 className="primary-heading footer-text-color">Hello</h1>
+        <div className="footer-columns">
+          <div className="col">
+            <div className="row">
+              <h1 className="secondary-heading footer-text-color">
+                ajad jewerly shop
+              </h1>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <h1 className="">col2</h1>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <h5 className="secondary-heading">Síguenos!</h5>
+              <div>
+                <InstagramIcon fontSize="large" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
